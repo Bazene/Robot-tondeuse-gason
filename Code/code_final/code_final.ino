6b3612440e6e0e5c9a544d200d62f9d486a653a2
BAZENE SERGE Amos <bazenesergeamos0@gmail.com>
2024-06-15 12:11:54 +0200

    Add the file final_code

--- Code/code_final/code_final.ino
+++ Code/code_final/code_final.ino
@@ -7,81 +7,134 @@
     to the current flowing through the motor, which can be used for overload detection 
     or other diagnostics.
 */
+#include <RobojaxBTS7960.h> // librairy for manage motors
 
+//************************************************************ BACK MOTORS **********************************************************************
 // for motor 1
-#define RPWM_1 3 // define pin 3 for RPWM pin (output)
-#define R_EN_1 27 // define pin 27 for R_EN pin (input)
+const int RPWM_1 = 3; // define pin 3 for RPWM pin (output)
+const int R_EN_1 = 27; // define pin 27 for R_EN pin (input)
 #define R_IS_1 A4 // define pin A4 for R_IS pin (output), 
 
-#define LPWM_1 4 // define pin 4 for LPWM pin (output)
-#define L_EN_1 31 // define pin 31 for L_EN pin (input)
+const int LPWM_1 = 4 ; // define pin 4 for LPWM pin (output)
+const int L_EN_1 = 31; // define pin 31 for L_EN pin (input)
 #define L_IS_1 A5 // define pin A5 for L_IS pin (output) ; Current Sense, ou détection de courant
 
 // for motor 2
-#define RPWM_2 6 // define pin 6 for RPWM pin (output)
-#define R_EN_2 39 // define pin 39 for R_EN pin (input)
+const int RPWM_2 = 6; // define pin 6 for RPWM pin (output)
+const int R_EN_2 = 39; // define pin 39 for R_EN pin (input)
 #define R_IS_2 A6 // define pin A6 for R_IS pin (output)
 
-#define LPWM_2 7 // define pin 7 for LPWM pin (output)
-#define L_EN_2 35 // define pin 35 for L_EN pin (input)
+const int LPWM_2 = 7; // define pin 7 for LPWM pin (output)
+const int L_EN_2 = 35; // define pin 35 for L_EN pin (input)
 #define L_IS_2 A7 // define pin A7 for L_IS pin (output)
 
 // variables that help us to control trafic
-#define CW 1 //do not change this
-#define CCW 0 //do not change this
-#define debug 0 //change to 0 to hide serial monitor debugging infornmation or set to 1 to view
+const int CW = 1; //do not change this
+const int CCW = 0; //do not change this
+const int debug = 0; //change to 0 to hide serial monitor debugging infornmation or set to 1 to view
 
-#include <RobojaxBTS7960.h>
+// instances for our tows motors
 RobojaxBTS7960 motor1(R_EN_1,RPWM_1,R_IS_1, L_EN_1,LPWM_1,L_IS_1,debug); //define motor 1 object (first instance of RobojaxBTS7960)
-RobojaxBTS7960 motor2(R_EN_2,RPWM_2,R_IS_2, L_EN_2,LPWM_2,L_IS_2,debug); //define motor 2 object and the same way for other motors (second instance of RobojaxBTS7960)
+RobojaxBTS7960 motor2(R_EN_2,RPWM_2,R_IS_2, L_EN_2,LPWM_2,L_IS_2,debug); //define motor 2 object (second instance of RobojaxBTS7960)
 
 bool motorRunningArriere = false; // State of motors
 
-void setup() {
-  // BTS7960 Motor Control Code by Robojax.com 20190622
-  Serial.begin(9600);// setup Serial Monitor to display information
+//************************************************************ TONTE MOTOR **********************************************************************
+//const int Mt_tonte = 10;
 
-  // initialisation of our tow motors
+//********************************************************* ULRASONIC SENSORS *******************************************************************
+// for front ultrasonic
+const int ECHO1 = 10;
+const int TRIG1 = 43;
+
+// for back ultrasonic
+const int ECHO2 = 13;
+const int TRIG2 = 47;
+
+const int obstacleLimite = 30; // the distance that help us to know if we can stop the robot (30cm because this will help us turn right or left without move back)
+
+//*********************************************************** SETUP FUNCTION *******************************************************************
+void setup() {
+  Serial.begin(9600); // setup Serial Monitor to display information
+
+  // Initialize motors sensors
   motor1.begin();
   motor2.begin();
+
+  // Initialize ultrasonics sensors
+  pinMode(ECHO1, INPUT); // receiver
+  pinMode(TRIG1, OUTPUT); // transmitter
+  pinMode(ECHO2, INPUT);
+  pinMode(TRIG2, OUTPUT);
+
+  // set default values for ultrasonic sensors
+  digitalWrite(TRIG1, LOW);
+  digitalWrite(TRIG2, LOW);
+
+  delay(2); // shot delay for stability
 }
 
-void loop() {
-//    motor1.rotate(50,CW);// run motor with 100% speed in CW direction
-//    motor2.rotate(50,CW);
-//    delay(5000);//run for 5 seconds
-//    
+//*************************************** FUNCTION TO CALCULATE THE DISTANCE USING ULTRASONIC ******************************************************
+long getDistance(int trigPin, int echoPin) {
+  // we send an impulsion TRIG of 10 microsecondes
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  
+  // We Mesure the length of ECHO impulsion
+  long duration = pulseIn(echoPin, HIGH);
+  
+  // Calcul of distance in cm
+  long distance = duration / 58.2;
+  
+  return distance;
+}
 
-    if(!motorRunningArriere) {
-      motor1.stop();// stop the motor
+//************************************* FUNCTION TO CONTROL THE ROBOT'S MOVEMENT AUTOMATICALLY ************************************************
+void movement_control(){
+  // Calcul of distance for each sensor
+  long distanceFront = getDistance(TRIG1, ECHO1);
+  long distanceBack = getDistance(TRIG2, ECHO2);
+
+  // default state of our motors
+  motor1.stop();
+  motor2.stop();
+  
+  if (distanceFront != 0 && distanceBack != 0) { // Ensure ultrasonic sensors are working
+    if(distanceFront > obstacleLimite) {
+      // run motor with 50% speed in CW direction
+      motor1.rotate(50,CW);
+      motor2.rotate(50,CW);
+    } else {
+       // we stop motors of 1 seconde
+       motor1.stop();
+       motor2.stop();
+       delay(1000); 
+
+       motor1.rotate(70,CW); // Turn right by running motor1 faster
+       motor2.rotate(50,CW); 
+
+       motor1.stop();
+       motor2.stop();
+       delay(1000); // Stop motors for 1 second
+ 
+       motor1.rotate(50,CW); // Resume forward movement
+       motor2.rotate(50,CW);
+    }
+
+    // If obstacle is too close at the back, stop motors for 1 second
+    if(distanceBack < obstacleLimite) {
+      motor1.stop();
       motor2.stop();
+      delay(1000); // Stop motors for 1 second
     }
-//    delay(3000);// stop for 3 seconds
-//    
-//    motor1.rotate(50,CCW);// run motor at 100% speed in CCW direction
-//    motor2.rotate(50,CCW);
-//    delay(5000);// run for 5 seconds
-//
-//    motor1.stop();// stop the motor
-//    motor2.stop();
-//    delay(3000);// stop for 3 seconds
+  }
+}
 
-    if (Serial.available() > 0) {
-    //gestion moteur tonte 
-    String receveidData = Serial.readStringUntil('\n'); // Lire la chaîne jusqu'au retour à la ligne
 
-      // ******************** FOR LAUNCHING BACK MOTORS OR FOR BEFORE MOVEMENT ***********************
-      if(receveidData == "turn on m arriere") {
-        motor1.rotate(50,CW);// run motor with 100% speed in CW direction
-        motor2.rotate(50,CW);
-        motorRunningArriere = true;
-      }
-
-      if(receveidData == "turn off m arriere") {
-        motor1.stop();// stop the motor
-        motor2.stop();
-        motorRunningArriere = false;
-      }
-    }
-  delay(2000);// stop for 3 seconds 
+//*********************************************************** LOOP FUNCTION *******************************************************************
+void loop() { 
+  movement_control();
+    
+  delay(2000); // reproduice the same processusse after 2 seconds 
 }
